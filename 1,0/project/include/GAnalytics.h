71cf0231955a5613fef96f04946cabb5dc67f573
mourlamstudios <mourlamstudios@gmail.com>
2012-05-14 23:41:22 +0000

    1.01 update

--- 1,0/project/include/GAnalytics.h
+++ 1,0/project/include/GAnalytics.h
@@ -5,11 +5,13 @@ namespace ganalytics
 {
 	#ifdef IPHONE
 		void setDebugMode(int deb);
+		void setCustomVar(int index,const char*name,const char *value);
         void startTracker(const char *acountID,int dispatchPeriod);
         void trackPageView(const char *pageName);
         void trackEvent(const char *category,const char *action,const char *label,int value);
         void dispatchTracker();
         void stopTracker();
+        void setupGACrashReporting();
     #endif
 }
 
